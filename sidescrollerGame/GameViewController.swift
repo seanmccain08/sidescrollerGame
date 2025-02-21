afcf5bdebd6a4cab57c8d6780e4e21d88ba9330a
seanmccain08 <seanmccain327@gmail.com>
2025-02-21 08:02:45 -0600

    started making a game

--- sidescrollerGame/GameViewController.swift
+++ sidescrollerGame/GameViewController.swift
@@ -14,6 +14,9 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+                    UIDevice.current.setValue(value, forKey: "orientation")
+        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
@@ -32,11 +35,9 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+       
+        return .landscapeRight
+        
     }
 
     override var prefersStatusBarHidden: Bool {
