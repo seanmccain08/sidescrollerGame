7f69c66ffd1e7e5d5ca7272b6dcba582fea7189e
seanmccain08 <seanmccain327@gmail.com>
2025-02-21 07:30:51 -0600

    Initial Commit

---
+++ sidescrollerGameTests/sidescrollerGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  sidescrollerGameTests.swift
+//  sidescrollerGameTests
+//
+//  Created by SEAN MCCAIN on 2/21/25.
+//
+
+import Testing
+@testable import sidescrollerGame
+
+struct sidescrollerGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
